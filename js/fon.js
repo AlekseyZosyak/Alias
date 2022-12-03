c3faebf74140be81de9a81d58789152e992ab155
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2022-12-03 15:29:26 +0200

    Update fon.js

--- js/fon.js
+++ js/fon.js
@@ -2,15 +2,15 @@ const FON = document.querySelector('body');
 FON.style.backgroundColor = '#C1EFFF';
 
 const element = [
-    { name: 'ball', src: '/fon/ball.svg' },
-    { name: 'cloud1', src: '/fon/left1.svg' },
-    { name: 'cloud2', src: '/fon/left2.svg' },
-    { name: 'cloud3', src: '/fon/right3.svg' },
-    { name: 'cloud4', src: '/fon/right4.svg' },
-    { name: 'cloud5', src: '/fon/right5.svg' },
-    { name: 'floor1', src: '/fon/floor1.svg' },
-    { name: 'floor2', src: '/fon/floor2.svg' },
-    { name: 'floor3', src: '/fon/floor3.svg' }
+    { name: 'ball', src: 'fon/ball.svg' },
+    { name: 'cloud1', src: 'fon/left1.svg' },
+    { name: 'cloud2', src: 'fon/left2.svg' },
+    { name: 'cloud3', src: 'fon/right3.svg' },
+    { name: 'cloud4', src: 'fon/right4.svg' },
+    { name: 'cloud5', src: 'fon/right5.svg' },
+    { name: 'floor1', src: 'fon/floor1.svg' },
+    { name: 'floor2', src: 'fon/floor2.svg' },
+    { name: 'floor3', src: 'fon/floor3.svg' }
 ];
 
 const ball = document.createElement('img');
